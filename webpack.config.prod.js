45b077ec28293142e0f6a268a72efa24ab55dda3
Thabo <zestadaryzor@gmail.com>
2019-11-26 08:58:42 +0200

    Update webpack.config.prod.js

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -37,7 +37,7 @@ module.exports = {
   ],
 
   resolve: {
-    extensions: ['', '.js', '.jsx'],
+    extensions: ['.js', '.jsx'],
     "alias": {
       "react": "preact-compat",
       "react-dom": "preact-compat"
